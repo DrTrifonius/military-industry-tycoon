7ea7da1667213f8b2d66b6251d571bdd4b1edf1f
Jonas 'Trifonius <etherealcommander@gmail.com>
2025-09-17 20:34:01 +0200

    scaffolding

--- dev-env.js
+++ dev-env.js
@@ -1,16 +1,16 @@
 const prod = false;
-setDevEnv()
+//setDevEnv()
 
 
-function setDevEnv(){
+function setDevEnv() {
 	document.querySelector("head").insertAdjacentHTML("beforeend", '\
 		<link type="text/css" rel="stylesheet" href="./dev.css" />');
 	sessionStorage.setItem("dev", true)
 }
 window.setDevEnv = setDevEnv
 const log = (str) => {
-    if (!prod && sessionStorage.getItem("dev")) {
-        console.log(str)
-    }
+	if (!prod && sessionStorage.getItem("dev")) {
+		console.log(str)
+	}
 }
 window.log = log
